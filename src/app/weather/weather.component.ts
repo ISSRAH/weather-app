ff6a19cf978e9802f824d854ddd5f34664e5468d
ISSRAH <sethsrajan12@gmail.com>
2019-06-02 12:35:31 +0530

    initial code changes

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,16 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
-  private baseUrl: string;
   private result: any;
-  private city: string;
-  public  cities = [
+  private city: any;
+  private weatherBaseUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+  public cities = [
     {Name: 'Pune'},
     {Name: 'Mumbai'},
     {Name: 'Bina'},
@@ -19,16 +18,28 @@ export class WeatherComponent implements OnInit {
     {Name: 'Delhi'}
   ];
 
-  constructor(private httpClient: HttpClient) {}
-
-  ngOnInit() {
-    this.baseUrl = 'http://api.openweathermap.org/data/2.5/weather?q=pune,india&APPID=213f3a8c13345c52f690a971e7909fd2';
-    this.get_products();
+  constructor(private httpClient: HttpClient) {
   }
 
-  get_products() {
-    this.httpClient.get(this.baseUrl).subscribe((res) => {
+  ngOnInit() {
+  }
+
+  getTemperatureByCityName(city: string) {
+    this.httpClient.get(this.weatherBaseUrl + city + '&APPID=213f3a8c13345c52f690a971e7909fd2&units=metric').subscribe((res) => {
       this.result = res.main.temp;
     });
   }
+
+  getWeatherCondition(): string {
+    if (this.result < 20) {
+      return 'cool';
+    }
+    if (this.result > 20 && this.result < 40) {
+      return 'moderate';
+    }
+    if (this.result > 40) {
+      return 'hot';
+    }
+  }
 }
+
