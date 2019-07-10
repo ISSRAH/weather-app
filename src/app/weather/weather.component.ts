1dce496d461c9cc6f20ad01d99391a52899eacd0
ISSRAH <sethsrajan12@gmail.com>
2019-07-10 16:33:35 +0530

    changes completed

--- src/app/weather/weather.component.ts
+++ src/app/weather/weather.component.ts
@@ -1,16 +1,17 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-
 @Component({
   selector: 'app-weather',
   templateUrl: './weather.component.html',
   styleUrls: ['./weather.component.css']
 })
 export class WeatherComponent implements OnInit {
-  private baseUrl: string;
+  private showhide: boolean
+  private addCityFieldValue: string;
   private result: any;
-  private city: string;
-  public  cities = [
+  private city: any;
+  private weatherBaseUrl = 'http://api.openweathermap.org/data/2.5/weather?q=';
+  public cities = [
     {Name: 'Pune'},
     {Name: 'Mumbai'},
     {Name: 'Bina'},
@@ -19,16 +20,38 @@ export class WeatherComponent implements OnInit {
     {Name: 'Delhi'}
   ];
 
-  constructor(private httpClient: HttpClient) {}
+  constructor(private httpClient: HttpClient) {
+  }
 
   ngOnInit() {
-    this.baseUrl = 'http://api.openweathermap.org/data/2.5/weather?q=pune,india&APPID=213f3a8c13345c52f690a971e7909fd2';
-    this.get_products();
+    this.addCityFieldValue = 'jabalpur';
+    this.city = this.cities[0];
+    this.getTemperatureByCityName(this.city.Name);
   }
 
-  get_products() {
-    this.httpClient.get(this.baseUrl).subscribe((res) => {
-      this.result = res.main.temp;
+  getTemperatureByCityName(city: string) {
+    this.httpClient.get(this.weatherBaseUrl + city + '&APPID=213f3a8c13345c52f690a971e7909fd2&units=metric').subscribe((res) => {
+      this.result = res.main;
     });
   }
+
+  getWeatherCondition(): string {
+    if (this.result.temp < 20) {
+      return 'cool';
+    }
+    if (this.result.temp > 20 && this.result.temp < 40) {
+      return 'moderate';
+    }
+    if (this.result.temp > 40) {
+      return 'hot';
+    }
+  }
+
+  add() {
+    console.log(this.addCityFieldValue);
+    this.cities[this.cities.length] = {Name: this.addCityFieldValue};
+  }
 }
+
+
+
